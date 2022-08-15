53f1c486aa7c0329ee1dd64d058f3e3eca16dd42
John <johnmulvey48@yahoo.com>
2022-08-15 12:55:11 -0400

    git@github.com:Jmulvez/phase-3-metaprogramming-custom-errors.git

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  def initialize(triangle)
+    @triangle= triangle
+  end
+  def kind
+    if triangle.is_a?(triangle)
+      self.triangle = triangle
+      triangle = self
+    else
+      begin
+        raise TriangleError => StandardError
+        puts error.message
+  end
+  class TriangleError < StandardError
+    def message
+    end
+  end
 end
